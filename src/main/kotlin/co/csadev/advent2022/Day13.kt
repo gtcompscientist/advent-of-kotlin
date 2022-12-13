89a605e22189b81b4c2d9213b03382b196be4998
Charles Anderson <charles.anderson@shopify.com>
2022-12-13 01:26:31 -0500

    2022 Day 13 - **

--- src/main/kotlin/co/csadev/advent2022/Day13.kt
+++ src/main/kotlin/co/csadev/advent2022/Day13.kt
@@ -9,12 +9,86 @@
 package co.csadev.advent2022
 
 import co.csadev.adventOfCode.BaseDay
-import co.csadev.adventOfCode.Resources.resourceAsList
+import co.csadev.adventOfCode.Resources.resourceAsText
 
-class Day13(override val input: List<String> = resourceAsList("22day13.txt")) :
-    BaseDay<List<String>, Int, Int> {
 
-    override fun solvePart1() = 0
+class Day13(override val input: String = resourceAsText("22day13.txt")) :
+    BaseDay<String, Int, Int> {
 
-    override fun solvePart2() = 0
+    private val packets = input.split("\n\n").map { it.lines().map { l -> Packet(l) } }
+
+    override fun solvePart1(): Int {
+        var count = 0
+        packets.forEachIndexed { index, packets ->
+            if (packets.first() > packets.last()) {
+                count += index + 1
+            }
+        }
+        return count
+    }
+
+    override fun solvePart2(): Int {
+        var decoderKey = 1
+        val mPackets = packets.flatten().toMutableList().apply {
+            add(Packet("[[2]]"))
+            add(Packet("[[6]]"))
+            sort()
+            reverse()
+        }
+        mPackets.forEachIndexed { index, packet ->
+            if (packet.str == "[[2]]" || packet.str == "[[6]]") decoderKey *= index + 1
+        }
+        return decoderKey
+    }
+}
+
+class Packet(val str: String) : Comparable<Packet> {
+    private val children = mutableListOf<Packet>()
+    private var integer = true
+    var value = 0
+
+    init {
+        var packet = str
+        if (packet == "[]") {
+            value = -1
+        }
+        if (!packet.startsWith("[")) {
+            value = packet.toInt()
+        } else {
+            packet = packet.substring(1, packet.length - 1)
+            var depth = 0
+            var curr = ""
+            for (c in packet.toCharArray()) {
+                if (c == ',' && depth == 0) {
+                    children.add(Packet(curr))
+                    curr = ""
+                } else {
+                    depth += if (c == '[') 1 else if (c == ']') -1 else 0
+                    curr += c
+                }
+            }
+            if (curr != "") {
+                children.add(Packet(curr))
+            }
+            integer = false
+        }
+    }
+
+    override fun compareTo(other: Packet): Int {
+        if (integer && other.integer) {
+            return other.value - value
+        }
+        if (!integer && !other.integer) {
+            for (i in 0 until children.size.coerceAtMost(other.children.size)) {
+                val value = children[i].compareTo(other.children[i])
+                if (value != 0) {
+                    return value
+                }
+            }
+            return other.children.size - children.size
+        }
+        val lst1 = if (integer) Packet("[$value]") else this
+        val lst2 = if (other.integer) Packet("[" + other.value + "]") else other
+        return lst1.compareTo(lst2)
+    }
 }
--- src/test/kotlin/co/csadev/advent2022/Day13Test.kt
+++ src/test/kotlin/co/csadev/advent2022/Day13Test.kt
@@ -10,7 +10,30 @@ import org.junit.jupiter.api.Test
 class Day13Test {
 
     private val input = """
-    """.trimIndent().lines()
+        [1,1,3,1,10]
+        [1,1,5,1,10]
+
+        [[1],[2,3,4]]
+        [[1],4]
+
+        [9]
+        [[8,7,6]]
+
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+
+        [7,7,7,7]
+        [7,7,7]
+
+        []
+        [3]
+
+        [[[]]]
+        [[]]
+
+        [1,[2,[3,[4,[5,6,7]]]],8,10]
+        [1,[2,[3,[4,[5,6,0]]]],8,10]
+    """.trimIndent()
 
     private val example = Day13(input)
     private val real = Day13()
@@ -18,18 +41,18 @@ class Day13Test {
     @Test
     fun `Matches example`() {
         val part1 = example.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(13)
 
         val part2 = example.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(140)
     }
 
     @Test
     fun `Actual answer`() {
         val part1 = real.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(5185)
 
         val part2 = real.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(23751)
     }
 }
