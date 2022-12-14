cc9f860056f69cff5db1e3a5fd7fc0c10e6106a6
Charles Anderson <charles.anderson@shopify.com>
2022-12-14 01:33:23 -0500

    Day 14 **

--- src/main/kotlin/co/csadev/advent2022/Day14.kt
+++ src/main/kotlin/co/csadev/advent2022/Day14.kt
@@ -9,12 +9,71 @@
 package co.csadev.advent2022
 
 import co.csadev.adventOfCode.BaseDay
+import co.csadev.adventOfCode.Point2D
 import co.csadev.adventOfCode.Resources.resourceAsList
+import kotlin.math.max
+
 
 class Day14(override val input: List<String> = resourceAsList("22day14.txt")) :
     BaseDay<List<String>, Int, Int> {
 
-    override fun solvePart1() = 0
+    override fun solvePart1() = solve()
 
-    override fun solvePart2() = 0
+    override fun solvePart2() = solve(false)
+
+    private var maxY = Int.MIN_VALUE
+
+    fun solve(part1: Boolean = true): Int {
+        val cave = MutableList(1000) { MutableList(1000) { false } }.apply {
+            input.forEach { line ->
+                addPath(line)
+            }
+        }
+        if (!part1) {
+            cave.addPath("0," + (maxY + 2) + " -> 999," + (maxY + 2))
+        }
+        var answer = 0
+        while (cave.addSand(maxY)) {
+            answer++
+        }
+        return answer
+    }
+
+    private fun MutableList<MutableList<Boolean>>.addPath(line: String) {
+        val rock = line.split(" -> ").map {
+            val (x, y) = it.split(",")
+            maxY = max(maxY, y.toInt())
+            Point2D(x.toInt(), y.toInt())
+        }
+        rock.windowed(2).forEach { (p1, p2) ->
+            val (x1, y1) = p1
+            val (x2, y2) = p2
+            for (j in x1.coerceAtMost(x2)..x1.coerceAtLeast(x2)) {
+                for (k in y1.coerceAtMost(y2)..y1.coerceAtLeast(y2)) {
+                    this[j][k] = true
+                }
+            }
+        }
+    }
+
+    private fun MutableList<MutableList<Boolean>>.addSand(maxY: Int): Boolean {
+        if (this[500][0]) {
+            return false
+        }
+        var x = 500
+        var y = 0
+        while (y <= maxY + 3) {
+            if (!this[x][y + 1]) {
+                y++
+            } else if (!this[x - 1][y + 1]) {
+                y++; x--
+            } else if (!this[x + 1][y + 1]) {
+                y++; x++
+            } else {
+                this[x][y] = true
+                return true
+            }
+        }
+        return false
+    }
 }
--- src/test/kotlin/co/csadev/advent2022/Day14Test.kt
+++ src/test/kotlin/co/csadev/advent2022/Day14Test.kt
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test
 class Day14Test {
 
     private val input = """
+        498,4 -> 498,6 -> 496,6
+        503,4 -> 502,4 -> 502,9 -> 494,9
     """.trimIndent().lines()
 
     private val example = Day14(input)
@@ -18,18 +20,18 @@ class Day14Test {
     @Test
     fun `Matches example`() {
         val part1 = example.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(24)
 
         val part2 = example.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(93)
     }
 
     @Test
     fun `Actual answer`() {
         val part1 = real.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(1001)
 
         val part2 = real.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(27976)
     }
 }
