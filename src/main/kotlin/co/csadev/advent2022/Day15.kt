79609a9aadc7ca5308fc2c8ec12858c127aae8de
Charles Anderson <charles.anderson@shopify.com>
2022-12-15 01:31:23 -0500

    Day 15 **

--- src/main/kotlin/co/csadev/advent2022/Day15.kt
+++ src/main/kotlin/co/csadev/advent2022/Day15.kt
@@ -1,20 +1,70 @@
-/*
- * Copyright (c) 2022 by Charles Anderson
- */
-
 /**
+ * Copyright (c) 2022 by Charles Anderson
  * Advent of Code 2022, Day 15
  * Problem Description: http://adventofcode.com/2021/day/15
  */
 package co.csadev.advent2022
 
 import co.csadev.adventOfCode.BaseDay
+import co.csadev.adventOfCode.Point2D
 import co.csadev.adventOfCode.Resources.resourceAsList
+import kotlin.math.max
+import kotlin.math.min
 
 class Day15(override val input: List<String> = resourceAsList("22day15.txt")) :
-    BaseDay<List<String>, Int, Int> {
+    BaseDay<List<String>, Int, Long> {
 
-    override fun solvePart1() = 0
+    private var sensors = mutableMapOf<Point2D, Point2D>()
+    private var allPoints = mutableSetOf<Point2D>()
+    private var minX: Int = Int.MAX_VALUE
+    private var maxX: Int = Int.MIN_VALUE
+    var p1Line = 2_000_000
+    var p2Max = 4000000L
 
-    override fun solvePart2() = 0
+    init {
+        input.forEach {
+            val (sensor, beacon) = it.split(":")
+            val s = sensor.asPoint
+            val b = beacon.asPoint
+            allPoints.add(s)
+            allPoints.add(b)
+            sensors[s] = b
+        }
+        minX = sensors.minOf { (s, b) -> min(s.x, b.x) }
+        maxX = sensors.maxOf { (s, b) -> max(s.x, b.x) }
+    }
+
+    // Arbitrary distance added to each side to ensure we don't miss anything.
+    override fun solvePart1() = (minX - p1Line * 5..maxX + p1Line * 5).asSequence()
+        .map { Point2D(it, p1Line) }
+        .filter { it !in allPoints }
+        .count { it.tooClose }
+
+    override fun solvePart2() =
+        sensors.mapNotNull { (s, b) -> scanOuter(s, s.distanceTo(b)) }.first()
+
+    private val String.asPoint: Point2D
+        get() = Point2D(
+            substringBefore(",").substringAfter("=").toInt(),
+            substringAfter("y=").toInt()
+        )
+
+    private val Point2D.tooClose: Boolean
+        get() = sensors.any { (s, b) -> distanceTo(s) <= s.distanceTo(b) }
+
+    private val Point2D.isBeacon: Boolean
+        get() = x in (0..p2Max) && y in (0..p2Max) && this !in allPoints && !tooClose
+
+    private val explore = listOf(Point2D(1, 1), Point2D(-1, 1), Point2D(-1, -1), Point2D(1, -1))
+
+    private fun scanOuter(s: Point2D, dist: Int): Long? {
+        var cur = s + Point2D(0, -dist - 1)
+        for (p in explore) for (step in 0..dist) {
+            cur += p
+            if (cur.isBeacon) {
+                return (cur.x * 4_000_000L) + cur.y
+            }
+        }
+        return null
+    }
 }
--- src/test/kotlin/co/csadev/advent2022/Day15Test.kt
+++ src/test/kotlin/co/csadev/advent2022/Day15Test.kt
@@ -10,6 +10,20 @@ import org.junit.jupiter.api.Test
 class Day15Test {
 
     private val input = """
+        Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+        Sensor at x=9, y=16: closest beacon is at x=10, y=16
+        Sensor at x=13, y=2: closest beacon is at x=15, y=3
+        Sensor at x=12, y=14: closest beacon is at x=10, y=16
+        Sensor at x=10, y=20: closest beacon is at x=10, y=16
+        Sensor at x=14, y=17: closest beacon is at x=10, y=16
+        Sensor at x=8, y=7: closest beacon is at x=2, y=10
+        Sensor at x=2, y=0: closest beacon is at x=2, y=10
+        Sensor at x=0, y=11: closest beacon is at x=2, y=10
+        Sensor at x=20, y=14: closest beacon is at x=25, y=17
+        Sensor at x=17, y=20: closest beacon is at x=21, y=22
+        Sensor at x=16, y=7: closest beacon is at x=15, y=3
+        Sensor at x=14, y=3: closest beacon is at x=15, y=3
+        Sensor at x=20, y=1: closest beacon is at x=15, y=3
     """.trimIndent().lines()
 
     private val example = Day15(input)
@@ -17,19 +31,21 @@ class Day15Test {
 
     @Test
     fun `Matches example`() {
+        example.p1Line = 10
         val part1 = example.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(26)
 
+        example.p2Max = 20
         val part2 = example.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(56_000_011L)
     }
 
     @Test
     fun `Actual answer`() {
         val part1 = real.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(5_403_290)
 
         val part2 = real.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(10_291_582_906_626L)
     }
 }
