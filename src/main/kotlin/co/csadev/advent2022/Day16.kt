e16d554fb7d185666a62fb5885df02d7fc7a2789
Charles Anderson <charles.anderson@shopify.com>
2022-12-16 15:59:38 -0500

    Day 16 ** Allow for arbitrary number of workers to open valves, simplify logic.

--- src/main/kotlin/co/csadev/advent2022/Day16.kt
+++ src/main/kotlin/co/csadev/advent2022/Day16.kt
@@ -52,8 +52,8 @@ class Day16(override val input: List<String> = resourceAsList("22day16.txt")) :
     private val currFlow: Int
         get() = open.filter { it.value }.mapNotNull { flows[it.key] }.sum()
 
-    private fun List<Int>.findPath(time: Int, pos: String, pos2: String? = null) {
-        val key = "$time$pos$pos2"
+    private fun List<Int>.findPath(time: Int, vararg pos: String) {
+        val key = "$time${pos.joinToString("")}"
         val currSum = sum()
         if (seen.getOrDefault(key, -1) >= currSum) {
             return
@@ -62,30 +62,32 @@ class Day16(override val input: List<String> = resourceAsList("22day16.txt")) :
         if (time == maxTime) {
             max = max(max, currSum)
         } else if (open.all { it.value }) {
-            (this + currFlow).findPath(time + 1, pos, pos2)
+            (this + currFlow).findPath(time + 1, *pos)
         } else {
-            searchPath(time, pos, pos2)
+            searchPath(time, 0, *pos)
         }
     }
 
-    private fun List<Int>.searchPath(time: Int, pos: String, pos2: String?, first: Boolean = true) {
-        val opening = if (!first && pos2 != null) pos2 else pos
+    private fun List<Int>.searchPath(time: Int, offset: Int, vararg pos: String) {
+        val opening = pos.getOrNull(offset) ?: return
         if (open[opening] != true && flows.getOrDefault(opening, 0) > 0) {
             open[opening] = true
-            addDepth(currFlow, pos, pos2, time, first)
+            addDepth(currFlow, time, offset + 1, *pos)
             open[opening] = false
         }
         val currentFlow = currFlow
         valves[opening]?.forEach { p ->
-            addDepth(currentFlow, if (first) p else pos, if (first) pos2 else p, time, first)
+            val subSearch = pos.toMutableList().apply { this[offset] = p }
+            addDepth(currentFlow, time, offset + 1, *subSearch.toTypedArray())
         }
     }
 
-    private fun List<Int>.addDepth(startFlow: Int, pos1: String, pos2: String?, time: Int, first: Boolean) {
-        if (pos2 != null && first) {
-            searchPath(time, pos1, pos2, false)
+    private fun List<Int>.addDepth(startFlow: Int, time: Int, offset: Int, vararg pos: String) {
+        val pos2 = pos.getOrNull(offset)
+        if (pos2 != null && offset > 0) {
+            searchPath(time, offset, *pos)
         } else {
-            (this + startFlow).findPath(time + 1, pos1, if (first) null else pos2)
+            (this + startFlow).findPath(time + 1, *pos)
         }
     }
 }
