f8900a7aa3589b809b22f46ce316be1789e2c800
Charles Anderson <charles.anderson@shopify.com>
2022-12-19 10:00:00 -0500

    Day 19 Switch to using Ints instead of lists, reduced runtime by 80%

--- src/main/kotlin/co/csadev/advent2022/Day19.kt
+++ src/main/kotlin/co/csadev/advent2022/Day19.kt
@@ -9,6 +9,7 @@ import co.csadev.adventOfCode.BaseDay
 import co.csadev.adventOfCode.Resources.resourceAsList
 import co.csadev.adventOfCode.copyApply
 import co.csadev.adventOfCode.product
+import kotlin.math.max
 
 class Day19(override val input: List<String> = resourceAsList("22day19.txt")) :
     BaseDay<List<String>, Int, Int> {
@@ -21,51 +22,44 @@ class Day19(override val input: List<String> = resourceAsList("22day19.txt")) :
         Cost(l[0], l[1], l[2], l[3], l[4], l[5], l[6])
     }
 
+    private val maxLowend = costs.maxOf { max(it.oO, it.cO) }
+    private val maxClay = costs.maxOf { it.obC }
+    private val maxOb = costs.maxOf { it.gOb }
+
     override fun solvePart1(): Int {
-        return costs.sumOf { it.runSim(24) * it.id }
+        return costs.sumOf { it.runSim(24, 1, 0, 0, 0, 0, 0, 0, 0) * it.id }
     }
 
     override fun solvePart2(): Int {
-        return costs.take(3).map { it.runSim(32) }.product()
+        return costs.take(3).map { it.runSim(32, 1, 0, 0, 0, 0, 0, 0, 0) }.product()
     }
 
-    companion object {
-        private const val Ore = 0
-        private const val Clay = 1
-        private const val Obsidian = 2
-        private const val Geode = 3
-    }
-
-    private fun Cost.runSim(
-        time: Int,
-        bots: MutableList<Int> = MutableList(4) { 0 }.apply { this[Ore] = 1 },
-        res: MutableList<Int> = MutableList(4) { 0 }
-    ): Int {
-        val nextRes = (0 until 4).map { res[it] + bots[it] }.toMutableList()
+    private fun Cost.runSim(time: Int, bOre: Int, bClay: Int, bOb: Int, bGeo: Int, rOre: Int, rClay: Int, rOb: Int, rGeo: Int): Int {
+        val (nOre, nClay, nOb, nGeo) = listOf(rOre + bOre, rClay + bClay, rOb + bOb, rGeo + bGeo)
         val nextTime = time - 1
 
         // Build as many Geode bots as you can
         return if (nextTime == 0) {
-            nextRes[Geode]
-        } else if (res[Ore] >= gO && res[Obsidian] >= gOb) {
-            runSim(nextTime, bots.apply { this[Geode]++ }, nextRes.apply { this[Ore] -= gO; this[Obsidian] -= gOb })
-        } else if (res[Ore] >= obO && res[Clay] >= obC) {
+            nGeo
+        } else if (rOre >= gO && rOb >= gOb) {
+            runSim(nextTime, bOre, bClay, bOb, bGeo + 1, nOre - gO, nClay, nOb - gOb, nGeo)
+        } else if (bOb < maxOb && rOre >= obO && rClay >= obC) {
             // Then build as many obsidian robots as you can
-            runSim(nextTime, bots.apply { this[Obsidian]++ }, nextRes.apply { this[Ore] -= obO; this[Clay] -= obC })
+            runSim(nextTime, bOre, bClay, bOb + 1, bGeo, nOre - obO, nClay - obC, nOb, nGeo)
         } else {
             // Past here we need to see what makes sense to build of the 3 options
             val options = mutableListOf<Int>()
             // Option 1: Build nothing
-            if (res[Ore] < 4) {
-                options += runSim(nextTime, bots.toMutableList(), nextRes.toMutableList())
+            if (rOre < maxLowend) {
+                options += runSim(nextTime, bOre, bClay, bOb, bGeo, nOre, nClay, nOb, nGeo)
             }
             // Option 2: Build an Ore robot
-            if (res[Ore] >= oO) {
-                options += runSim(nextTime, bots.copyApply { this[Ore] += 1 }, nextRes.copyApply { this[Ore] -= oO })
+            if (bClay < maxClay && rOre >= oO) {
+                options += runSim(nextTime, bOre + 1, bClay, bOb, bGeo, nOre - oO, nClay, nOb, nGeo)
             }
             // Option 3: Build a Clay robot
-            if (res[Ore] >= cO) {
-                options += runSim(nextTime, bots.copyApply { this[Clay] += 1 }, nextRes.copyApply { this[Ore] -= cO })
+            if (rOre >= cO) {
+                options += runSim(nextTime, bOre, bClay + 1, bOb, bGeo, nOre - cO, nClay, nOb, nGeo)
             }
             options.maxOf { it }
         }
