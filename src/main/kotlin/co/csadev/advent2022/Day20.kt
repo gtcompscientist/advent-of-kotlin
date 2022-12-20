ba220d02c3fab0f75cd968ba62e404d748c3d5e4
Charles Anderson <charles.anderson@shopify.com>
2022-12-20 01:06:12 -0500

    Day 20 **

--- src/main/kotlin/co/csadev/advent2022/Day20.kt
+++ src/main/kotlin/co/csadev/advent2022/Day20.kt
@@ -9,13 +9,28 @@ import co.csadev.adventOfCode.BaseDay
 import co.csadev.adventOfCode.Resources.resourceAsList
 
 class Day20(override val input: List<String> = resourceAsList("22day20.txt")) :
-    BaseDay<List<String>, Int, Int> {
+    BaseDay<List<String>, Long, Long> {
 
-    override fun solvePart1(): Int {
-        return 0
-    }
+    private val decryptionKey = 811589153
 
-    override fun solvePart2(): Int {
-        return 0
+    override fun solvePart1() = mix()
+
+    override fun solvePart2() = mix(decryptionKey, 10)
+
+    private fun mix(decryptKey: Int = 1, mix: Int = 1): Long {
+        val base = input.mapIndexed { index, i -> index to i.toLong() * decryptKey }
+        val mixing = base.toMutableList()
+        repeat(mix) {
+            base.forEach { p ->
+                val index = mixing.indexOf(p)
+                mixing.removeAt(index)
+                // Order matters here, must calculate the new index after removal
+                val newIndex = (index + p.second).mod(mixing.size)
+                mixing.add(newIndex, p)
+            }
+        }
+        val output = mixing.map { it.second }
+        val zero = output.indexOf(0L)
+        return output.run { this[(zero + 1000) % size] + this[(zero + 2000) % size] + this[(zero + 3000) % size] }
     }
 }
--- src/test/kotlin/co/csadev/advent2022/Day20Test.kt
+++ src/test/kotlin/co/csadev/advent2022/Day20Test.kt
@@ -10,6 +10,13 @@ import org.junit.jupiter.api.Test
 class Day20Test {
 
     private val input = """
+        1
+        2
+        -3
+        3
+        -2
+        0
+        4
     """.trimIndent().lines()
 
     private val example = Day20(input)
@@ -18,18 +25,18 @@ class Day20Test {
     @Test
     fun `Matches example`() {
         val part1 = example.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(3L)
 
         val part2 = example.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(1623178306L)
     }
 
     @Test
     fun `Actual answer`() {
         val part1 = real.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(2275L)
 
         val part2 = real.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(4090409331120L)
     }
 }
