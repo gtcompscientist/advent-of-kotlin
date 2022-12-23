ce220ae6ef4679d436c14be9d251be7f4a7fb0ce
Charles Anderson <charles.anderson@shopify.com>
2022-12-23 01:39:20 -0500

    Day 23 **

--- src/main/kotlin/co/csadev/advent2022/Day23.kt
+++ src/main/kotlin/co/csadev/advent2022/Day23.kt
@@ -6,12 +6,75 @@
 package co.csadev.advent2022
 
 import co.csadev.adventOfCode.BaseDay
+import co.csadev.adventOfCode.Point2D
 import co.csadev.adventOfCode.Resources.resourceAsList
+import co.csadev.adventOfCode.printArea
+import kotlin.math.abs
 
 class Day23(override val input: List<String> = resourceAsList("22day23.txt")) :
     BaseDay<List<String>, Int, Int> {
 
-    override fun solvePart1() = 0
+    private val elvesStart = input.flatMapIndexed { y, s -> s.mapIndexedNotNull { x, c -> if (c == '#') Point2D(x, y) else null } }.toSet()
 
-    override fun solvePart2() = 0
+    private val northern = listOf(Point2D(0, -1), Point2D(1, -1), Point2D(-1, -1))
+    private val southern = listOf(Point2D(0, 1), Point2D(1, 1), Point2D(-1, 1))
+    private val western = listOf(Point2D(-1, 0), Point2D(-1, -1), Point2D(-1, 1))
+    private val eastern = listOf(Point2D(1, 0), Point2D(1, -1), Point2D(1, 1))
+    private val directionList = listOf(northern, southern, western, eastern)
+
+    override fun solvePart1(): Int {
+        val state = elvesStart.toMutableSet()
+        val dirs = directionList.toMutableList()
+//        state.print()
+        repeat(10) {
+            state.moveElves(dirs)
+            dirs.add(dirs.removeAt(0))
+//            state.print()
+        }
+        val ySize = abs(state.maxOf { it.y } - state.minOf { it.y }) + 1
+        val xSize = abs(state.maxOf { it.x } - state.minOf { it.x }) + 1
+        return (xSize * ySize) - state.size
+    }
+
+    @Suppress("unused")
+    private fun Set<Point2D>.print() {
+        val min = Point2D(minOf { p -> p.x }, minOf { p -> p.y })
+        val max = Point2D(maxOf { p -> p.x }, maxOf { p -> p.y })
+        printArea(min, max) { c, _ -> if (c) '#' else '.' }
+        println()
+    }
+
+    override fun solvePart2(): Int {
+        val state = elvesStart.toMutableSet()
+        val dirs = directionList.toMutableList()
+//        var num = 0
+        return generateSequence {
+            state.moveElves(dirs).also {
+                dirs.add(dirs.removeAt(0))
+//                state.print()
+            }
+        }.takeWhile {
+//            println("Round ${num++}")
+            it
+        }.count() + 1
+    }
+
+    private fun MutableSet<Point2D>.moveElves(dirList: List<List<Point2D>>): Boolean {
+        var moved = false
+        val proposal = mutableMapOf<Point2D, MutableList<Point2D>>()
+        for (p in this) {
+            if (p.neighbors.none { n -> n in this }) continue
+
+            dirList.firstOrNull { dir ->
+                dir.map { n -> n + p }.none { p -> p in this }
+            }?.let { propDir ->
+                (proposal.getOrPut(p + propDir.first()) { mutableListOf() }) += p
+            }
+        }
+        proposal.filter { it.value.size == 1 }.forEach { (newP, oldP) ->
+            moved = true
+            if (remove(oldP[0])) add(newP)
+        }
+        return moved
+    }
 }
--- src/test/kotlin/co/csadev/advent2022/Day23Test.kt
+++ src/test/kotlin/co/csadev/advent2022/Day23Test.kt
@@ -10,6 +10,13 @@ import org.junit.jupiter.api.Test
 class Day23Test {
 
     private val input = """
+        ....#..
+        ..###.#
+        #...#.#
+        .#...##
+        #.###..
+        ##.#.##
+        .#..#..
     """.trimIndent().lines()
 
     private val example = Day23(input)
@@ -18,18 +25,18 @@ class Day23Test {
     @Test
     fun `Matches example`() {
         val part1 = example.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(110)
 
         val part2 = example.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(20)
     }
 
     @Test
     fun `Actual answer`() {
         val part1 = real.solvePart1()
-        Assertions.assertThat(part1).isEqualTo(0)
+        Assertions.assertThat(part1).isEqualTo(3906)
 
         val part2 = real.solvePart2()
-        Assertions.assertThat(part2).isEqualTo(0)
+        Assertions.assertThat(part2).isEqualTo(895)
     }
 }
