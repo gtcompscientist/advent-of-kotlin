e6909011915aceed2ae872f4f8de7a9f7383c415
Charles Anderson <charles.s.anderson@gmail.com>
2022-12-23 19:34:54 -0500

    Refactor day 4 to be more Kotlin-y.

--- src/main/kotlin/co/csadev/advent2022/Day04.kt
+++ src/main/kotlin/co/csadev/advent2022/Day04.kt
@@ -11,19 +11,11 @@ import co.csadev.adventOfCode.Resources.resourceAsList
 class Day04(override val input: List<String> = resourceAsList("22day04.txt")) :
     BaseDay<List<String>, Int, Int> {
 
-    private fun String.toRangePair() = split(",")
-        .map { e ->
-            e.split("-")
-                .map { it.toInt() }
-                .run { (this[0]..this[1]).toSet() }
-        }
-
-    override fun solvePart1() = input.count { p ->
-        val ranges = p.toRangePair()
-        ranges[0].containsAll(ranges[1]) || ranges[1].containsAll(ranges[0])
+    private val ranges = input.map {
+        it.split(",", "-").chunked(2).map { (i1, i2) -> (i1.toInt()..i2.toInt()).toSet() }
     }
 
-    override fun solvePart2() = input.count { p ->
-        p.toRangePair().reduce { acc, ran -> acc.intersect(ran) }.isNotEmpty()
-    }
+    override fun solvePart1() = ranges.count { (r1, r2) -> r1.containsAll(r2) || r2.containsAll(r1) }
+
+    override fun solvePart2() = ranges.count { (r1, r2) -> r1.intersect(r2).isNotEmpty() }
 }
--- src/test/kotlin/co/csadev/advent2022/Day04Test.kt
+++ src/test/kotlin/co/csadev/advent2022/Day04Test.kt
@@ -10,12 +10,12 @@ import org.junit.jupiter.api.Test
 class Day04Test {
 
     private val input = """
-2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-6-6,4-6
-2-6,4-8
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8
     """.trimIndent().lines()
 
     private val example = Day04(input)
